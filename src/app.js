0ed3f47567dac1a12465da11fd4afc6310528329
kendy-karakawa <kendy.karakawa@gmail.com>
2023-01-07 08:59:42 -0300

    bonus varidação de dados

--- src/app.js
+++ src/app.js
@@ -11,7 +11,6 @@ const users = [
     avatar:
       "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
   },
-
 ];
 
 const tweets = [
@@ -24,22 +23,23 @@ const tweets = [
 app.post("/sign-up", (req, res) => {
   const user = req.body;
   if (!user.username || !user.avatar)
-    return res.status(422).send("Preencha todos os campos");
+    return res.status(400).send("Todos os campos são obrigatórios!");
   users.push(user);
   res.send("ok");
 });
 
 app.post("/tweets", (req, res) => {
-  const tweet = req.body;
-  const checkUser = users.find((item) => item.username === tweet.username);
+  const tweeted = req.body;
+  if(!tweeted.username || !tweeted.tweet) return res.status(400).send("Todos os campos são obrigatórios!")
+  const checkUser = users.find((item) => item.username === tweeted.username);
   if (!checkUser) return res.send("UNAUTHORIZED");
 
-  tweets.push(tweet);
+  tweets.push(tweeted);
   res.send("OK");
 });
 
 app.get("/tweets", (req, res) => {
-  const last10Tweets = tweets.slice(-3, tweets.length);
+  const last10Tweets = tweets.slice(-10, tweets.length);
   for (let i = 0; i < last10Tweets.length; i++) {
     for (let j = 0; j < users.length; j++) {
       if (last10Tweets[i].username === users[j].username) {
