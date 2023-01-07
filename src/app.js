484b09fba618d2dec7c961b5d70b9f7694d1463e
kendy-karakawa <kendy.karakawa@gmail.com>
2023-01-07 08:28:32 -0300

    get /tweets - retorna ultimaos 10 twwets ok

--- src/app.js
+++ src/app.js
@@ -2,42 +2,53 @@ import express from "express";
 import cors from "cors";
 
 const app = express(); // Cria um servidor
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-const users = [{
-	username: 'bobesponja', 
-	avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info" 
-}]
+const users = [
+  {
+    username: "bobesponja",
+    avatar:
+      "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+  },
 
-const tweets = [{
-	username: "bobesponja",
-  tweet: "eu amo o hub"
-}]
+];
 
-app.post("/sign-up", (req, res)=>{
-  const user = req.body
-  console.log(user)
-  if(!user.username || !user.avatar) return res.status(422).send("Preencha todos os campos")
-  users.push(user)
-  res.send("ok")
+const tweets = [
+  {
+    username: "bobesponja",
+    tweet: "eu amo o hub",
+  },
+];
 
-})
+app.post("/sign-up", (req, res) => {
+  const user = req.body;
+  if (!user.username || !user.avatar)
+    return res.status(422).send("Preencha todos os campos");
+  users.push(user);
+  res.send("ok");
+});
 
-app.post("/tweets", (req, res)=>{
-  const tweet = req.body
-  const checkUser = users.find(item => item.username === tweet.username)
-  if (!checkUser) return res.send("UNAUTHORIZED")
+app.post("/tweets", (req, res) => {
+  const tweet = req.body;
+  const checkUser = users.find((item) => item.username === tweet.username);
+  if (!checkUser) return res.send("UNAUTHORIZED");
 
-  tweets.push(tweet)
-  res.send("OK")
+  tweets.push(tweet);
+  res.send("OK");
+});
 
-})
+app.get("/tweets", (req, res) => {
+  const last10Tweets = tweets.slice(-3, tweets.length);
+  for (let i = 0; i < last10Tweets.length; i++) {
+    for (let j = 0; j < users.length; j++) {
+      if (last10Tweets[i].username === users[j].username) {
+        last10Tweets[i].avatar = users[j].avatar;
+      }
+    }
+  }
 
-
-// Configura uma função pra ser executada quando bater um GET na rota "/"
-app.get("/", (req, res) => {
-  res.send("hello word");
+  res.send(last10Tweets);
 });
 
 app.listen(5000);
