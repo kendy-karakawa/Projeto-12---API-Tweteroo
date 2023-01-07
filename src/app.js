43801d63dfa5f7c5f1d6c265422c85fcf8cf203b
kendy-karakawa <kendy.karakawa@gmail.com>
2023-01-07 08:01:36 -0300

    App Post sing-up e post twwet

--- src/app.js
+++ src/app.js
@@ -3,6 +3,36 @@ import cors from "cors";
 
 const app = express(); // Cria um servidor
 app.use(cors())
+app.use(express.json())
+
+const users = [{
+	username: 'bobesponja', 
+	avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info" 
+}]
+
+const tweets = [{
+	username: "bobesponja",
+  tweet: "eu amo o hub"
+}]
+
+app.post("/sign-up", (req, res)=>{
+  const user = req.body
+  console.log(user)
+  if(!user.username || !user.avatar) return res.status(422).send("Preencha todos os campos")
+  users.push(user)
+  res.send("ok")
+
+})
+
+app.post("/tweets", (req, res)=>{
+  const tweet = req.body
+  const checkUser = users.find(item => item.username === tweet.username)
+  if (!checkUser) return res.send("UNAUTHORIZED")
+
+  tweets.push(tweet)
+  res.send("OK")
+
+})
 
 
 // Configura uma função pra ser executada quando bater um GET na rota "/"
